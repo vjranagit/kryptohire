e4e2eb6d1bb769ac04c03c548aceb0437a5fac19
vjrana <mr.usman.mustafa@gmail.com>
2026-02-20 16:39:09 -0500

    Fix ESLint unused-vars error in buy-me-coffee.tsx

--- src/components/shared/buy-me-coffee.tsx
+++ src/components/shared/buy-me-coffee.tsx
@@ -1,10 +1,7 @@
 'use client';
 
-interface BuyMeCoffeeProps {
-  className?: string;
-}
-
-// Removed: previously contained third-party Buy Me Coffee widget
-export function BuyMeCoffee({ className: _className }: BuyMeCoffeeProps) {
+// Removed: previously contained third-party Buy Me Coffee widget (alexfromvan)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function BuyMeCoffee(_props: { className?: string }) {
   return null;
 }
